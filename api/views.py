246587313aa3570667b9798a55959a36baaddc64
italo11marcos <italomarcosboa@gmail.com>
2021-05-21 17:02:17 -0300

    Modificando viewset

--- api/views.py
+++ api/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
 
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 from api.models import Author, Book
 from api.serializers import AuthorSerializer, BookSerializer
 
@@ -7,6 +10,12 @@ from api.serializers import AuthorSerializer, BookSerializer
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+    @action(detail=True, methods=['get'])
+    def books(self, request, pk=None):
+        author = self.get_object()
+        serializer = BookSerializer(Book.objects.filter(author=author), many=True)
+        return Response(serializer.data)
 
 
 class BookViewSet(viewsets.ModelViewSet):
