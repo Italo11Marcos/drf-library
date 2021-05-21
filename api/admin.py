a47c9d55b0674d096c8f23614f0540bd4537dbec
italo11marcos <italomarcosboa@gmail.com>
2021-05-21 13:14:06 -0300

    Add admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from api.models import Author, Book
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'isbn', 'language', 'num_pages']
