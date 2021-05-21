c234bd80d25417c0e47c49f025264ef2aee874e8
italo11marcos <italomarcosboa@gmail.com>
2021-05-21 14:28:59 -0300

    Add relationships

--- api/serializers.py
+++ api/serializers.py
@@ -15,6 +15,25 @@ class AuthorSerializer(serializers.ModelSerializer):
 
 class BookSerializer(serializers.ModelSerializer):
 
+    #Nested Relationship
+    author = AuthorSerializer(many=True, read_only=True)
+    
+
+    #HypedLinked Related Field
+    """
+    author = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='author-detail'
+    )
+    """
+
+    #Primary Key Related Field
+    """
+    author = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    """
+
+
     class Meta:
         model = Book
         fields = (
